fa3b3f31b47fe42cf91c31b4bd491bef1d6ddefb
Rizwan Fahim <rizwanfahim011@outlook.com>
2024-12-25 20:29:20 +0500

    Update Browse.jsx

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -3,7 +3,7 @@ import Navbar from "./shared/Navbar";
 import Job from "./Job";
 import { useDispatch, useSelector } from "react-redux";
 import { setSearchedQuery } from "@/redux/jobSlice";
-import useGetAllJobs from '../hooks/useGetAllJobs'; // Ensure this path is correct
+import useGetAllJobs from '../Hooks/useGetAllJobs'; // Ensure this path is correct
 
 
 // const randomJobs = [1, 2,45];
